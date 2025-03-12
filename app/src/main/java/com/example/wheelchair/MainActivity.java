c3239247de5443503e57e6e0fbbfc31c5da19e2b
uygar <uygarb00@gmail.com>
2025-03-12 20:35:17 +0300

    Initial push

---
+++ app/src/main/java/com/example/wheelchair/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.wheelchair;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.wheelchair.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Button button = binding.button;
+
+        button.setOnClickListener(view -> {
+            Toast.makeText(this, "SELAMINALEYKUM", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
